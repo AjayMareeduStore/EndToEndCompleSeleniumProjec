b05fc10b32d605631bba330d71213d029ec634f0
AjayMareedu <ajaymareedu666@gmail.com>
2025-12-23 20:06:13 +0530

    this is new update from Ajay

--- src/RealTimeExample/DemoForGit.java
+++ src/RealTimeExample/DemoForGit.java
@@ -3,7 +3,8 @@ package RealTimeExample;
 public class DemoForGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+  System.out.println("this is ajay");
 
 	}
 
